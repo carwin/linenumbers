b2676275eb64830f2474ed0134269a73241baac0
Carwin Young <carwinyoung@gmail.com>
2012-10-14 14:01:46 -0500

    Main javascript file sets the margins for each line number to the same as the element of the line it numbers. Pretty sneaky sis.

---
+++ linenumbers_block_style.js
@@ -0,0 +1,22 @@
+(function ($) {
+  Drupal.behaviors.linenumbers = {
+    attach: function(context, settings) {
+      var margins = [];
+      var elementArray = $('.ln_fl').children().map(function(){
+        console.log($(this));
+        var text_margins = ($(this).css('margin')); 
+        margins.push(text_margins);
+      });
+      var numberArray = $('aside').children();
+      $.each(numberArray, function(index, value){
+        console.log(margins[index]);
+        $(this).css({
+          'margin' : margins[index],
+          'display' : 'block',
+          'margin-left' : '0',
+          'margin-right' : '0',
+        });
+      });
+    }
+  }
+}) (jQuery);
